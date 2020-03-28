b5e440ec7ad39a35ceb2f4583d38ff2634b5bbff
HazardousHenkie <thekyle@hotmail.com>
2020-03-28 16:21:44 +0900

    adding drawer

--- src/components/Atoms/Drawer/styledComponents.ts
+++ src/components/Atoms/Drawer/styledComponents.ts
@@ -1,13 +1,18 @@
 import styled from 'styled-components'
 
-export const HandleBar = styled.div`
-    position: fixed;
+interface HandleBarProps {
+    relative?: boolean
+}
+
+export const HandleBar = styled.div<HandleBarProps>`
+    position: ${(props): string => (props.relative ? 'relative' : 'fixed')};
     border-radius: 100px;
-    border: 3px solid ${(props): string => props.theme.black};
-    margin: 0 auto 8px;
+    border: 2px solid ${(props): string => props.theme.black};
+    margin: ${(props): string =>
+        props.relative ? '-5px auto 10px' : '0 auto 8px'};
     width: 50%;
     bottom: 0;
-    left: 25%;
+    left: ${(props): string => (!props.relative ? '25%' : '0')};
     top: auto;
     background: ${(props): string => props.theme.black};
 `
