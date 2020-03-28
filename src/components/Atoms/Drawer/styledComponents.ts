ef70ae8b0dddd488efe15f21c77e9ef83313c8e1
HazardousHenkie <thekyle@hotmail.com>
2020-03-28 17:51:47 +0900

    drawer add icon

--- src/components/Atoms/Drawer/styledComponents.ts
+++ src/components/Atoms/Drawer/styledComponents.ts
@@ -1,8 +1,26 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp'
 
 interface HandleBarProps {
     relative?: boolean
 }
+
+const mouseAnimation = keyframes`
+  0% {
+    transform: translate(0, 0);
+    opacity: 0;
+  }
+  40% {
+    opacity: 1;
+  }
+  80% {
+    transform: translate(0, -10px);
+    opacity: 0;
+  }
+  100% {
+    opacity: 0;
+  }
+`
 
 export const HandleBar = styled.div<HandleBarProps>`
     position: ${(props): string => (props.relative ? 'relative' : 'fixed')};
@@ -20,4 +38,25 @@ export const HandleBar = styled.div<HandleBarProps>`
 export const DrawerWrapper = styled.div`
     position: relative;
     bottom: 0;
+`
+
+export const SwipeIndicator = styled.div`
+    opacity: 0.5;
+    position: absolute;
+    left: 50%;
+    width: 30px;
+    height: 30px;
+    margin-left: -15px;
+    border: 2px solid ${(props): string => props.theme.black};
+    border-radius: 50px;
+    bottom: 12px;
+`
+
+export const SwipeIndicatorInner = styled(ArrowDropUpIcon)`
+    position: absolute;
+    top: 5px;
+    left: 50%;
+    margin-left: -12px;
+    color: ${(props): string => props.theme.black};
+    animation: ${mouseAnimation} 2s infinite;
 `
