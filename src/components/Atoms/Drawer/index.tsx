21fc3a2c7b3c534f6c2593040f242ae156d0f570
HazardousHenkie <thekyle@hotmail.com>
2020-03-28 16:21:44 +0900

    adding drawer

--- src/components/Atoms/Drawer/index.tsx
+++ src/components/Atoms/Drawer/index.tsx
@@ -37,7 +37,7 @@ const Drawer: React.FC<Omit<
                 onClose={toggleDrawer(false)}
                 onOpen={toggleDrawer(true)}
             >
-                <HandleBar />
+                <HandleBar relative />
 
                 {children}
             </SwipeableDrawer>
