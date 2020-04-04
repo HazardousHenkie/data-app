d9672fa582a6de71992384181affca13adc7eada
HazardousHenkie <thekyle@hotmail.com>
2020-04-04 19:11:43 +0900

    countries list

--- src/components/Molecules/HeaderSearch/index.tsx
+++ src/components/Molecules/HeaderSearch/index.tsx
@@ -1,11 +1,29 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import SearchField from 'components/Molecules/SearchField'
+import CountriesList from 'components/Molecules/CountriesList'
+
+import TopBar from './styledComponents'
 
 const HeaderSearch: React.FC = () => {
     const [searchString, setSearchString] = useState('')
+    const [openCountriesList, setOpenCountriesList] = React.useState(false)
 
-    return <SearchField setValue={setSearchString} value={searchString} />
+    useEffect(() => {
+        if (searchString !== '') {
+            setOpenCountriesList(true)
+        }
+    }, [searchString])
+
+    return (
+        <TopBar>
+            <SearchField setValue={setSearchString} />
+            <CountriesList
+                open={openCountriesList}
+                setOpen={setOpenCountriesList}
+            />
+        </TopBar>
+    )
 }
 
 export default HeaderSearch
