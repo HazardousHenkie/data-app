df0b707a5c216cf1334816918f17255452c0cc59
HazardousHenkie <thekyle@hotmail.com>
2020-04-08 12:11:29 +0900

    changing

--- src/components/Molecules/CountriesList/constants.ts
+++ src/components/Molecules/CountriesList/constants.ts
@@ -1,7 +1,8 @@
 enum ActionTypes {
-    GET_COUNTRIES_DATA = 'componens/CountriesList/GET_COUNTRIES_DATA',
-    GET_COUNTRIES_DATA_SUCCESS = 'componens/CountriesList/GET_COUNTRIES_DATA_SUCCESS',
-    GET_COUNTRIES_DATA_ERROR = 'componens/CountriesList/GET_COUNTRIES_DATA_ERROR'
+    SET_SELECTED_COUNTRY = 'components/CountriesList/SET_SELECTED_COUNTRY',
+    GET_COUNTRIES_DATA = 'components/CountriesList/GET_COUNTRIES_DATA',
+    GET_COUNTRIES_DATA_SUCCESS = 'components/CountriesList/GET_COUNTRIES_DATA_SUCCESS',
+    GET_COUNTRIES_DATA_ERROR = 'components/CountriesList/GET_COUNTRIES_DATA_ERROR'
 }
 
 export default ActionTypes
--- src/containers/HomePage/styledComponents.ts
+++ src/containers/HomePage/styledComponents.ts
@@ -5,7 +5,7 @@ import { Map, MapProps } from 'react-leaflet'
 const StyledMap = styled(Map)<MapProps>`
     width: 100%;
     height: 100vh;
-    filter: ${(props: any) => props.theme.mapFilter};
+    filter: ${(props): string => props.theme.mapFilter};
 `
 
 export default StyledMap
