58b58b51c060036e67958dc84e1db53a4f20e449
HazardousHenkie <thekyle@hotmail.com>
2020-04-19 18:39:42 +0900

    adding advisory

---
+++ src/containers/HomePage/Organisms/DrawerCountryContent/styledComponents.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Header = styled.h1`
+    font-size: 2rem;
+    text-align: center;
+    margin: 0 0 10px;
+`
+
+export default Header
--- src/containers/HomePage/index.tsx
+++ src/containers/HomePage/index.tsx
@@ -6,21 +6,13 @@ import { useTranslation } from 'react-i18next'
 import DrawerContext from 'components/Organisms/Drawer/DrawerContext'
 import Drawer from 'components/Organisms/Drawer'
 
-import { createSelector } from 'reselect'
-import { useSelector } from 'react-redux'
-
-import makeSelectCountry from 'containers/HomePage/Molecules/CountryListItem/selectors'
 import HeaderSearch from './Organisms/HeaderSearch'
+import DrawerCountryContent from './Organisms/DrawerCountryContent'
 
 import OSMap from './map'
 
-const stateSelector = createSelector(makeSelectCountry(), country => ({
-    country
-}))
-
 const HomePage: React.FC = () => {
     const { t } = useTranslation('homePage')
-    const { country } = useSelector(stateSelector)
     const [openDrawer, setOpenDrawer] = useState(false)
 
     return (
@@ -38,7 +30,9 @@ const HomePage: React.FC = () => {
             <DrawerContext.Provider value={{ openDrawer, setOpenDrawer }}>
                 <OSMap />
 
-                <Drawer>{country.name}</Drawer>
+                <Drawer>
+                    <DrawerCountryContent />
+                </Drawer>
             </DrawerContext.Provider>
         </>
     )
