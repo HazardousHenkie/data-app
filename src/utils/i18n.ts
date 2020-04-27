b5aebbd9ff1341bc6228c0a53461b698eb0958ed
HazardousHenkie <thekyle@hotmail.com>
2020-04-27 21:49:53 +0900

    test error fix

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,5 +1,5 @@
 import i18n from 'i18next'
-import Backend from 'i18next-xhr-backend'
+import Backend from 'i18next-http-backend'
 import LanguageDetector from 'i18next-browser-languagedetector'
 import { initReactI18next } from 'react-i18next'
 
@@ -9,9 +9,6 @@ i18n.use(Backend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
-        backend: {
-            loadPath: '/locales/{{lng}}/{{ns}}.json'
-        },
         debug: false,
         lng: 'en',
         fallbackLng: 'en',
