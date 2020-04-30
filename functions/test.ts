91314f66b6ca84a4c60bc0be9d9002fd9c6dc75d
HazardousHenkie <thekyle@hotmail.com>
2020-04-30 19:08:50 +0900

    first lambda

--- functions/test.ts
+++ functions/test.ts
@@ -10,11 +10,6 @@ const handler: Handler = (
     context: Context,
     callback: Callback
 ) => {
-    // "event" has information about the path, body, headers, etc. of the request
-    console.log('event', event)
-    // "context" has information about the lambda environment and user details
-    console.log('context', context)
-    // The "callback" ends the execution of the function and returns a response back to the caller
     const response: Response = {
         statusCode: 200,
         body: JSON.stringify({
