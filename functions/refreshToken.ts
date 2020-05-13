967d87d0a5daeb507d762b7b9a95b6e3d487c74f
HazardousHenkie <thekyle@hotmail.com>
2020-05-13 19:59:08 +0900

    starting refresh token

--- functions/refreshToken.ts
+++ functions/refreshToken.ts
@@ -1,7 +1,77 @@
-const placeholder = () => {}
+import jwt from 'jsonwebtoken'
+
+import cookie from 'cookie'
+
+import { Handler, Context, Callback, APIGatewayEvent } from 'aws-lambda'
+
+import { ExprArg } from 'faunadb'
+import publicKey from './keys/publicKeyRefresh'
+
+interface Response {
+    statusCode: number
+    headers?: object
+    body: string
+}
+
+interface UserResponseInterface {
+    ref: ExprArg
+    ts: number
+    data: Record<string, string>
+}
+
+const handler: Handler = (
+    event: APIGatewayEvent,
+    context: Context,
+    callback: Callback
+) => {
+    let response: Response
+
+    if (event.body && JSON.parse(event.body).authToken) {
+        // get id from somewhere maybe the acces token jwt
+        // check for the user
+
+        try {
+            const payload = jwt.verify(
+                cookie.parse(event.headers.cookie).jwt_refresh,
+                publicKey
+            ) as Record<string, string | number>
+
+            console.log(payload)
+
+            response = {
+                statusCode: 200,
+                headers: {
+                    'Set-Cookie': [
+                        // createJwtCookie(
+                        //     existingUser.data.googleId,
+                        //     existingUser.data.name
+                        // )
+                    ],
+                    'Content-Type': 'application/json'
+                },
+                body: 'new auth token succesfully created'
+            }
+        } catch (error) {
+            response = { statusCode: 400, body: JSON.stringify(error.message) }
+        }
+    } else {
+        response = {
+            statusCode: 422,
+            body: "Event body wasn't correct or refreshToken wasn't found."
+        }
+    }
+
+    return callback(null, response)
+}
+
+// eslint-disable-next-line import/prefer-default-export
+export { handler }
+
 // get id from somewhere maybe the acces token jwt
 // check for the user
+
 // check validity of refresh jwt
+
 // check if id and token are in db
 // if everything is fine create new token
 // console.log('before', refreshToken)
@@ -9,4 +79,3 @@ const placeholder = () => {}
 // const existingRefreshToken = await getToken(refreshToken, googleUser.sub)
 
 // https://github.com/afteracademy/nodejs-backend-architecture-typescript/blob/master/src/routes/v1/access/token.ts
-export default placeholder
