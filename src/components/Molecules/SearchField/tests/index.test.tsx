fe2d368ba4df614af312e1ef1c927d4c83df4d9f
HazardousHenkie <thekyle@hotmail.com>
2020-07-08 10:35:07 +0900

    adding tests

--- src/components/Molecules/SearchField/tests/index.test.tsx
+++ src/components/Molecules/SearchField/tests/index.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render } from 'utils/test-utils'
+import { render, fireEvent } from 'utils/test-utils'
 
 import SearchField from '../index'
 
-// finish this one
+// check use effect testing
 describe('<SearchField />', () => {
     it('should render like snapshot', () => {
         const component = render(<SearchField setValue={() => {}} />)
@@ -11,10 +11,39 @@ describe('<SearchField />', () => {
         expect(component).toMatchSnapshot()
     })
 
-    it('should render an <input> tag', () => {
+    it('should render an <form> ', () => {
+        const { container } = render(<SearchField setValue={() => {}} />)
+
+        const input = container.querySelector('form')
+        expect(input?.tagName).toEqual('FORM')
+    })
+
+    it('should render an <input> tag if translation is ready', () => {
         const { container } = render(<SearchField setValue={() => {}} />)
 
         const input = container.querySelector('input')
         expect(input?.tagName).toEqual('INPUT')
+    })
+
+    it('It should render the translation tag', () => {
+        const { getByPlaceholderText } = render(
+            <SearchField setValue={() => {}} />
+        )
+
+        const buttonText = getByPlaceholderText('search')
+
+        expect(buttonText).toBeInTheDocument()
+    })
+
+    it('should render an <input> tag', () => {
+        const { container } = render(<SearchField setValue={() => {}} />)
+
+        const input = container.querySelector('input')
+
+        fireEvent.change(input as Element, {
+            target: { value: 'newValue' }
+        })
+
+        expect(input).toHaveProperty('value', 'newValue')
     })
 })
