006557920fe0a2d7a3c500de82cc0b6c776d9669
HazardousHenkie <thekyle@hotmail.com>
2020-07-24 21:55:21 +0900

    adding error text

--- src/containers/HomePage/Organisms/Errors/tests/index.test.tsx
+++ src/containers/HomePage/Organisms/Errors/tests/index.test.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from 'utils/test-utils'
 
+import history from 'utils/history'
+import configureStore from 'store/configureStore'
+
+import { removeError } from 'globals/globalErrors/actions'
+
 import ErrorSnackbars from '../index'
 
 describe('<ErrorSnackbars />', () => {
@@ -8,5 +13,48 @@ describe('<ErrorSnackbars />', () => {
         const component = render(<ErrorSnackbars />)
 
         expect(component).toMatchSnapshot()
+    })
+
+    it('should render Snackbar', () => {
+        const { getByTestId } = render(<ErrorSnackbars />, {
+            initialState: { errors: { errors: [Error('testError')] } }
+        })
+
+        const Snackbar = getByTestId('Snackbar')
+
+        expect(Snackbar).toBeInTheDocument()
+    })
+
+    it('should render InfoMessage with message inside Snackbar', () => {
+        const { getByText } = render(<ErrorSnackbars />, {
+            initialState: { errors: { errors: [Error('testError')] } }
+        })
+
+        const Snackbar = getByText('testError')
+
+        expect(Snackbar).toBeInTheDocument()
+    })
+
+    it('should render Snackbar with style', () => {
+        const { getByTestId } = render(<ErrorSnackbars />, {
+            initialState: { errors: { errors: [Error('testError')] } }
+        })
+
+        const Snackbar = getByTestId('Snackbar')
+
+        expect(Snackbar).toHaveAttribute('style', 'top: 0px;')
+    })
+
+    it('should call handleClose when is closed', () => {
+        const mockStore = configureStore({}, history)
+        mockStore.dispatch = jest.fn()
+
+        const handleClose = jest.fn()
+
+        // @ts-ignore since it's for testing ignore it
+        render(<ErrorSnackbars onClose={handleClose()} />, { store: mockStore })
+
+        expect(handleClose).toHaveBeenCalledTimes(1)
+        expect(mockStore.getState().errors).toMatchObject({ errors: [] })
     })
 })
