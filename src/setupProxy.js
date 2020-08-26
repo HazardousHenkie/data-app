504e1482b49cce6a0dd3d74109f989a45f8cdd86
HazardousHenkie <thekyle@hotmail.com>
2020-08-26 19:41:00 +0900

    style fixes

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,13 +3,13 @@
 // const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = app => {
-//     app.use(
-//         createProxyMiddleware('/.netlify/functions/', {
-//             target: 'http://localhost:9000/',
-//             changeOrigin: true,
-//             pathRewrite: {
-//                 '^/\\.netlify/functions': ''
-//             }
-//         })
-//     )
+    //     app.use(
+    //         createProxyMiddleware('/.netlify/functions/', {
+    //             target: 'http://localhost:9000/',
+    //             changeOrigin: true,
+    //             pathRewrite: {
+    //                 '^/\\.netlify/functions': ''
+    //             }
+    //         })
+    //     )
 }
